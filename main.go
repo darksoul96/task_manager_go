05cbce87d885f7965987c03e98d015b43c529de1
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-06-11 16:01:07 -0300

    Added Gin Web Framework to handle requests

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe(":8080", nil)
-}
+// func main() {
+// 	fs := http.FileServer(http.Dir("static"))
+// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+// 	http.HandleFunc("/", rootHandler)
+// 	http.ListenAndServe(":8080", nil)
+// }
+//
+// // Handlers
+//
+// func rootHandler(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "index.html")
+// }
+//
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// http.Handle("/", http.FileServer(http.Dir("styles/")))
-	http.ServeFile(w, r, "index.html")
+func main() {
+	r := gin.Default()
+	r.Static("/static", "./static")
+	r.LoadHTMLFiles("index.html")
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run("localhost:8080")
 }
